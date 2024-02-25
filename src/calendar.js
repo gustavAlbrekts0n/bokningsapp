21726d1dd4863238feb299b941aa0db3232963f4
gustavAlbrekts0n <gustav.albrektson@gmail.com>
2024-02-25 11:54:02 +0100

    Add inital code

---
+++ src/calendar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import CalendarDays from './calendar-days';
+import './calendar.css';
+
+export default class Calendar extends Component {
+    constructor() {
+        super();
+
+        this.weekdays = ["Mån", "Tis", "Ons", "Tor", "Fre", "Lör", "Sön"];
+        this.months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"];
+
+        this.state = {
+            currentDay: new Date()
+        }
+    }
+
+    changeCurrentDay = (day) => {
+        this.setState({ currentDay: new Date(day.year, day.month, day.number) });
+    }
+
+    render() {
+        return (
+            <div className="calendar">
+                <div className="calendar-header">
+                    <h2>{this.months[this.state.currentDay.getMonth()]} {this.state.currentDay.getFullYear()}</h2>
+                </div>
+                <div className="calendar-body">
+                    <div className="table-header">
+                        {
+                            this.weekdays.map((weekday) => {
+                                return <div className="weekday"><p>{weekday}</p></div>
+                            })
+                        }
+                    </div>
+                    <CalendarDays day={this.state.currentDay} changeCurrentDay={this.changeCurrentDay} />
+                </div>
+            </div>
+        );
+    }
+}
