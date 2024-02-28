4915b0203f422fb1b4c6c7fcf6de37bce8276b88
gustav <gustav.albrektson@gmail.com>
2024-02-28 08:25:02 +0100

    Add time component and attribute if its booked or not

--- src/time-picker.js
+++ src/time-picker.js
@@ -1,37 +1,52 @@
-import React, { Component } from 'react';
 import './time-picker.css';
 
-export default class TimePicker extends Component {
-    constructor() {
-        super();
+const Time = ({ index, date, isBooked }) => {
+    const times = [
+        "07:00 - 11:00",
+        "11:00 - 14:00",
+        "14:00 - 17:00",
+        "17:00 - 21:00"
+    ];
+  
+    return (
+        <div className={"time" + (isBooked ? " booked" : "")}>
+            <p>{times[index]}</p>
+        </div>
+    );
+};
 
-        this.times = [
-            "07:00 - 11:00",
-            "11:00 - 14:00",
-            "14:00 - 17:00",
-            "17:00 - 21:00"
-        ];
+const TimePicker = ({ selectedDate }) => {
+    const times = [
+        "07:00 - 11:00",
+        "11:00 - 14:00",
+        "14:00 - 17:00",
+        "17:00 - 21:00"
+    ];
 
-        this.weekdays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
-        this.months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"];
-    }
+    const weekdays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
+    const months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"];
 
-    render() {
-        const date = this.props.selectedDate ? this.weekdays[this.props.selectedDate.weekday] + " " + this.props.selectedDate.number + " " + this.months[this.props.selectedDate.month] : "";
+    const date = selectedDate
+        ? `${weekdays[selectedDate.weekday]} ${selectedDate.number} ${months[selectedDate.month]}`
+        : "";
 
-        return ( 
-            <div className="time-picker">
-                <div className="selected-date">
-                    <p>{this.props.selectedDate ? date : "Välj datum"}</p>
-                </div>
-                <div className="time-list">
-                    {
-                        this.times.map((time) => {
-                            return <div className="time"><p>{time}</p></div>
-                        })
-                    }
-                </div>
+    return (
+        <div className="time-picker">
+            <div className="selected-date">
+                <p>{selectedDate ? date : 'Välj datum'}</p>
             </div>
-        );
-    }
-}
+            <div className="time-list">
+                {times.map((time, index) => (
+                    <Time
+                        key={index}
+                        index={index}
+                        date={selectedDate}
+                        isBooked={index % 2 !== 0}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default TimePicker;
