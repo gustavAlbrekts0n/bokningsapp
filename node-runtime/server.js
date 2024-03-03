ed008d453431330a7acfbb0ee9f0a7a1ea705061
gustav <gustav.albrektson@gmail.com>
2024-03-03 14:08:00 +0100

    Create initial database

--- node-runtime/server.js
+++ node-runtime/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const Datastore = require("nedb");
+
 const app = express();
 app.listen(8080, () => console.log("listening at 8080"));
 app.use(express.static("../react-app/build"));
 app.use(express.json());
+
+const database = new Datastore("database.db");
+database.loadDatabase();
+database.insert({date: "Today", isBooked: true});
+database.insert({date: "Tomorrow", isBooked: false});
 
 app.post("/api", (request, response) => {
     const data = request.body;
